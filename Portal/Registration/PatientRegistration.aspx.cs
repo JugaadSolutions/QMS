3507a047185e79b4e58b6dc8ca9cd20580cc1500
sushma-gargya <sushma.gargya@gmail.com>
2014-10-07 21:27:55 +0530

    validation done for registration
    Removed java script and used msgbox function to display registered token

--- Portal/Registration/PatientRegistration.aspx.cs
+++ Portal/Registration/PatientRegistration.aspx.cs
@@ -11,6 +11,8 @@ using Portal.DLayer;
 
 
 
+
+
 namespace Portal.Registration
 {
     public partial class PatientRegistration : System.Web.UI.Page
@@ -22,24 +24,35 @@ namespace Portal.Registration
             TextBox2.Text = "";
             da = new DataAccess();
         }
-
+       
         protected void Button1_Click(object sender, EventArgs e)
         {
+            
             
           int token = da.RegisterPatient(TextBox1.Text,TextBox2.Text);
           Token.Value = token.ToString();
             if(token != -1 )
             {
-                //MsgBox(token.ToString());
-                Response.Write("<script>alert('Registration Successfull your token number is  " + token + "');if(alert){ window.location='PatientRegistration.aspx';}</script>");
+               
+                MsgBox("Registration Successfull your token number is  " + token + "");
+               // Response.Write("<script>alert('Registration Successfull your token number is  " + token + "');</script>");
+                TextBox1.Text = "";
+                TextBox2.Text = "";
+                
             }
             else
             {
-                Response.Write("<script>alert('invalid registration')</script>");
+                MsgBox("invalid registration");
             }
 
         }
-       
+        void MsgBox(string strMsg)
+        {
+            Label lbl = new Label();
+            lbl.Text = "<script language='javascript'>" + Environment.NewLine + "window.alert(" + "'" + strMsg + "'" + ")</script>";
+            Page.Controls.Add(lbl);
+        }
 
     }
+    
 }
