24bc732cdf5a3b8b474a7f95aae96790e0354023
sushma-gargya <sushma.gargya@gmail.com>
2015-02-10 13:30:22 +0530

    Seed Method tested(sushma)

--- QMSPortal/QMS/Migrations/Configuration.cs
+++ QMSPortal/QMS/Migrations/Configuration.cs
@@ -19,12 +19,13 @@ namespace QMS.Migrations
             context.IPs.AddOrUpdate(i=>i.IP_Address,
 
                   new IP { Name = "Counter1", IP_Address = "192.168.1.3" },
-            new IP { Name = "Counter2", IP_Address = "116.202.114.159" }
+                  new IP { Name = "Counter2", IP_Address = "192.168.1.108" },
+                  new IP { Name = "Counter3", IP_Address = "116.202.114.159" }
                 );
             context.Configs.AddOrUpdate(t=>t.key,
-
-                    new Config { key = "Token", Value = 1 }
+                new Config { key = "Token", Value = 1 }
                     );
+                
             context.Users.AddOrUpdate(u=>u.Name,
 
                 new User { Name = "sa", Password = "123" }
