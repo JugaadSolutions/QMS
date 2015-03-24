1619222e4e84dd33ed4ab1b232f2dc8a434dd733
Ideonics-in <ideonics.in@gmail.com>
2015-03-24 17:12:34 +0530

    Removed Seed

--- QMSPortal/QMS/Migrations/Configuration.cs
+++ QMSPortal/QMS/Migrations/Configuration.cs
@@ -15,36 +15,36 @@ namespace QMS.Migrations
             ContextKey = "QMS.Models.QMS_db";
         }
 
-        protected override void Seed(QMS.Models.QMS_db context)
-        {
-            context.Database.ExecuteSqlCommand("TRUNCATE TABLE IPs ");
-            context.IPs.AddOrUpdate(i => i.IP_Address,
+        //protected override void Seed(QMS.Models.QMS_db context)
+        //{
+        //    context.Database.ExecuteSqlCommand("TRUNCATE TABLE IPs ");
+        //    context.IPs.AddOrUpdate(i => i.IP_Address,
 
-                  new IP { Name = "Counter1", IP_Address = "192.168.1.1" },
-                  new IP { Name = "Counter2", IP_Address = "192.168.1.2" },
-                  new IP { Name = "Counter3", IP_Address = "192.168.1.3" },
-                  new IP { Name = "Counter4", IP_Address = "192.168.1.4" },
-                  new IP { Name = "Counter5", IP_Address = "192.168.1.5" },
-                  new IP { Name = "Counter6", IP_Address = "192.168.1.6" }
-                );
-            context.Users.AddOrUpdate(u => u.Name,
+        //          new IP { Name = "Counter1", IP_Address = "192.168.1.1" },
+        //          new IP { Name = "Counter2", IP_Address = "192.168.1.2" },
+        //          new IP { Name = "Counter3", IP_Address = "192.168.1.3" },
+        //          new IP { Name = "Counter4", IP_Address = "192.168.1.4" },
+        //          new IP { Name = "Counter5", IP_Address = "192.168.1.5" },
+        //          new IP { Name = "Counter6", IP_Address = "192.168.1.6" }
+        //        );
+        //    context.Users.AddOrUpdate(u => u.Name,
 
-                new User { Name = "sa", Password = "123" }
-                );
-            var token =from r in context.Configs
-                     select r.Value;
-            foreach(var item in token)
-            {
-                return;
+        //        new User { Name = "sa", Password = "123" }
+        //        );
+        //    var token =from r in context.Configs
+        //             select r.Value;
+        //    foreach(var item in token)
+        //    {
+        //        return;
                 
-            }
-            context.Configs.AddOrUpdate(t => t.Id,
+        //    }
+        //    context.Configs.AddOrUpdate(t => t.Id,
 
-                    new Config { Id = 1, key = "Token", Value = 1 }
-                        );
+        //            new Config { Id = 1, key = "Token", Value = 1 }
+        //                );
                 
 
 
-        }
+        //}
     }
 }
